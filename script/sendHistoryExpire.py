0a358516de05cc27060327cd8968b8a9ae2eb130
atomiky <75833319+atomiky@users.noreply.github.com>
2021-12-04 13:06:43 +0800

    python send history expire mail

--- script/sendHistoryExpire.py
+++ script/sendHistoryExpire.py
@@ -7,12 +7,12 @@ from email.mime.text import MIMEText
 
 def send_email(receiver,expireTime):
     # 接收方／发送方，接收方是一个list，可以接受多个数值
-    sender = 'hi@nxkys.com'
+    sender = 'lingxi@oneky.xyz'
 
     contant = """<div dir="ltr">
-        ﻿亲亲，您账号的过期时间是：<font color="darkred">%s</font>
+        亲亲，您账号已过期，过期时间是：<font color="darkred">%s</font>
 		<br><br>
-		<font color="red">将于今天过期</font>，为不影响您正常使用，请及时续费<br><br>
+		为不影响您正常使用，请及时续费<br><br>
 		续费请 <a href="https://www.nxkys.com" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.nxkys.com&amp;source=gmail&amp;ust=1638599219093000&amp;usg=AOvVaw1_o00JKGkAtJHfAIdy040a">登录官网</a>，点击<font color="red">'购买套餐'</font> <br> <br>
 		续费后请<font color="red">稍等两分钟,</font>然后重新连接客户端 <br> <br>
 		有疑问，请联系网站右下角在线客服 <br> <br>
@@ -29,7 +29,7 @@ def send_email(receiver,expireTime):
     server = smtplib.SMTP_SSL('smtp.zoho.com', 465)
     try:
         # 登陆邮箱，发送邮件退出登陆
-        server.login('hi@nxkys.com', 'nx_Admin6688')
+        server.login('lingxi@oneky.xyz', 'lingxi_Admin6688')
         server.sendmail(sender, [receiver], message.as_string())
         server.quit()
     except smtplib.SMTPException:
@@ -48,8 +48,7 @@ if __name__ == '__main__':
     # SQL 查询语句
     sql = """SELECT email,class_expire
     FROM `user`
-    where class_expire > date(now())
-    and class_expire < DATE_ADD(date(now()),INTERVAL 1 DAY)"""
+    where class_expire < date(now())"""
 
     try:
        # 执行SQL语句
