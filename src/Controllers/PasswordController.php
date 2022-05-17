7864874f383658fd78e54a2976c15ade0b31c609
atomiky <75833319+atomiky@users.noreply.github.com>
2022-05-17 20:26:22 +0800

    解锁苹果账户

--- src/Controllers/PasswordController.php
+++ src/Controllers/PasswordController.php
@@ -68,7 +68,8 @@ class PasswordController extends BaseController
         $rs['ret'] = 1;
         $rs['msg'] = '重置邮件已经发送,请检查邮箱.';
         if (Password::sendResetEmail($email)) {
-            $res['msg'] = '邮件发送失败，请联系网站管理员。';
+//            $res['msg'] = '邮件发送失败，请联系网站管理员。';
+            $res['msg'] = '请联系右下角在线客服，修改密码';
         }
 
         return $response->getBody()->write(json_encode($rs));
