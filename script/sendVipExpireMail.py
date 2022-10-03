1eda13364075f0f56a01f51008021ba7abeb0a3d
awkys <awkys96@gmail.com>
2022-10-03 17:12:19 +0800

    vip expire mail

--- script/sendVipExpireMail.py
+++ script/sendVipExpireMail.py
@@ -7,16 +7,18 @@ from email.mime.text import MIMEText
 
 def send_email(receiver,expireTime):
     # 接收方／发送方，接收方是一个list，可以接受多个数值
-    sender = 'nxkyss@zohomail.jp'
+    sender = 'vip-expire@nxbest.men'
 
     contant = """<div dir="ltr">
-        ﻿亲亲，您账号的过期时间是：<font color="darkred">%s</font>
+        亲，您在本站购买的海外网络加速服务已过期。<br><br>
+        您账号的过期时间是：<font color="darkred">%s</font>
         <br><br>
         <font color="red">为不影响您正常使用，请登录网站购买套餐</font><br><br>
         续费请 <a href="https://nxkys.com" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://nxkys.com&amp;source=gmail&amp;ust=1638599219093000&amp;usg=AOvVaw1_o00JKGkAtJHfAIdy040a">登录官网</a>，点击<font color="red">'购买套餐'</font> <br> <br>
         续费后请<font color="red">稍等两分钟,</font>然后重新连接客户端 <br> <br>
         官方网址： <a href="https://nxkys.com" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://nxkys.com&amp;source=gmail&amp;ust=1638599219094000&amp;usg=AOvVaw2ZcvjoZ_OBnw_4TE1vJZHf">https://nxkys.com</a><div class="yj6qo"></div><div class="adL"> <br> <br>
         请复制官方地址去浏览器打开 <br> <br>
+        灵溪防走失网址： <a href="https://awkys.github.io" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://awkys.github.io&amp;source=gmail&amp;ust=1638599219094000&amp;usg=AOvVaw2ZcvjoZ_OBnw_4TE1vJZHf">https://awkys.github.io</a><div class="yj6qo"></div><div class="adL"> <br> <br>
         有疑问，请联系网站右下角在线客服 。<br> <br>
         </div></div>"""%(str(expireTime))
 
